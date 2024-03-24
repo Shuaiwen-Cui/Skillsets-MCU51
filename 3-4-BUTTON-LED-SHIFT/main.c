79bc76d5b67fd3d8750d921e944f2b1e947971ca
Shuaiwen-Cui <SHUAIWEN001@e.ntu.edu.sg>
2024-03-24 16:00:55 +0800

    auto commit by win-upload.bat

---
+++ 3-4-BUTTON-LED-SHIFT/main.c
@@ -0,0 +1,49 @@
+#include <REGX52.H>
+unsigned char LEDNum=0;
+void Delay(unsigned int xms)		//@11.0592MHz
+{
+	unsigned char i, j;
+    while(xms--)
+    {
+        //_nop_();
+        i = 2;
+        j = 199;
+        do
+        {
+            while (--j);
+        } while (--i);
+    }
+
+}
+
+int main()
+{
+    P2 = ~0x01;
+    while(1)
+    {
+        if(P3_1==0)
+        {
+            Delay(20);
+            while(P3_1==0);
+            Delay(20);
+            
+            LEDNum++;
+            if(LEDNum>=8)
+                LEDNum=0;
+            P2 = ~(0x01<<LEDNum);
+        }
+        if(P3_0==0)
+        {
+            Delay(20);
+            while(P3_0==0);
+            Delay(20);
+            
+            if(LEDNum == 0)
+                LEDNum = 7;
+            else
+                LEDNum--;
+            
+            P2 = ~(0x01<<LEDNum);
+        }
+    }
+}
